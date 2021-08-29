873317c0fcdf19b6d0326716a129a6368f0452dc
Helarga <elargah@gmail.com>
2021-08-29 12:41:43 +0200

    updated path.

--- addon/pycThermopack/makescript.py
+++ addon/pycThermopack/makescript.py
@@ -9,17 +9,17 @@ import sys
 from pathlib import Path
 
 parser = argparse.ArgumentParser()
-parser.add_argument("mode", type=str, help="optim or debug")
+parser.add_argument('mode', type=str, help='optim or debug')
 
 args = parser.parse_args()
 mode = args.mode
 
 # Glob to handle different OS suffixes
 libthermo = list(
-    Path('../../bin/dynamic').glob(f'libthermopack_{mode}_gfortran*.*'))[0]
+    Path('../../bin').glob(f'libthermopack_{mode}_gfortran*.*'))[0]
 
 if not os.path.exists(libthermo):
     print(f'{libthermo}does not exist. Have you compiled thermopack?')
     sys.exit(1)
 
-shutil.copy2(libthermo, "./pyctp/libthermopack.so")
+shutil.copy2(libthermo, './pyctp/libthermopack.so')
